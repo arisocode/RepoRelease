b71e35a83b9bbe5738c1ac64abc221a1c48df67f
arisocode <aricabrerac21@gmail.com>
2025-12-30 18:43:34 -0500

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RepoRelease",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="arisocode",                             # Tu nombre
+    author_email="aricabrerac21@gmail.com",         # Tu correo electrónico
+    url="https://github.com/arisocode/reporelease", # URL del proyecto
+)
